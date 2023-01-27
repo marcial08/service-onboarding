921b463d622198ba6ac1f5ad79008bd3e4e34f47
codefher <codefher@gmail.com>
2023-01-27 10:43:00 -0400

    cambio onboarding

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 //* Router
-app.use('/api', onboardingRoutes)
+app.use('/api/onboarding', onboardingRoutes)
 // app.use('/api/users', userRoutes)
 
 export default app
